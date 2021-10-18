b9d8646a2c869a657d3a11ec59ee8a179796a4a6
roy-finch <royfinch99@hotmail.com>
2021-10-18 19:58:28 +0000

    created a second app to manage the homepage and modified the settings to list the templates location to construct the index page and a base page.

--- retro_exchange/settings.py
+++ retro_exchange/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+
+    # My created apps.ALLOWED_HOSTS
+    'home',
 ]
 
 SITE_ID = 1
@@ -74,7 +77,10 @@ ROOT_URLCONF = 'retro_exchange.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
