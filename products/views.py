d9bba229f97705b799c172802d76cfb78a6540d3
roy-finch <royfinch99@hotmail.com>
2021-10-19 14:21:27 +0000

    added the three databases that will be used by the products page, and also added a html file, and view render.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def all_products(request):
+    """ This is the view for the products """
+
+    return render(request, "products/products.html")
