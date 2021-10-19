2398bcb1959c955cf3e18a06aa4ae5d6db70d47a
roy-finch <royfinch99@hotmail.com>
2021-10-19 14:23:16 +0000

    addded the information required to the admin file so that the databases can be altered and modified from the admin view

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from .models import Product, Category, Console
 
 # Register your models here.
+
+
+class ProductAdmin():
+    """
+    Alters the way data is
+    displayed in admin
+    """
+    list_display = {
+        "sku",
+        "name",
+        "price",
+        "console",
+        "category",
+        "rating",
+        "image"
+    }
+
+
+class CategoryAdmin():
+    """
+    Alters the way data is
+    displayed in admin
+    """
+    list_display = {
+        "name",
+        "display_name",
+    }
+
+
+class ConsoleAdmin():
+    """
+    Alters the way data is
+    displayed in admin
+    """
+    list_display = {
+        "name",
+        "display_name",
+    }
+
+
+"""
+Registers the alterations
+"""
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Console)
