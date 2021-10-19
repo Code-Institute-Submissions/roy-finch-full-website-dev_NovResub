7deab5c008ba94f298912ae5e8b7ca7c8561a691
roy-finch <royfinch99@hotmail.com>
2021-10-19 14:22:10 +0000

    added the model classes to the products.models so that the databases can be accessed with an according model class.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Model classes for the categories so
+    that they can be used to create new category types
+    also meta to correct the pluralisations
+    of the words categories from categorys
+    """
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    display_name = models.CharField(max_length=254, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.display_name
+
+
+class Console(models.Model):
+    """
+    Model class for the console so it can be
+    accessed and new entries can be added
+    """
+    name = models.CharField(max_length=254)
+    display_name = models.CharField(max_length=254, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.display_name
+
+
+class Product(models.Model):
+    """
+    Model class for the products so they can be
+    added and deleted
+    """
+    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    console = models.ForeignKey(
+        'Console', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    description = models.TextField()
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
