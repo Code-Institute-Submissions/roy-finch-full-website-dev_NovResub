7643baa43e6acc383663924c0a5dc708f50e796d
roy-finch <royfinch99@hotmail.com>
2021-10-19 15:15:41 +0000

    created a view for individual products and set up the urls so that you can click on a product and it will find the corrisponding product.

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -11,3 +11,13 @@ def all_products(request):
     }
 
     return render(request, "products/products.html", context)
+
+
+def product_detail(request, product_pk):
+    products = get_object_or_404(Product, pk=product_pk)
+
+    context = {
+        "products": products
+    }
+
+    return render(request, "products/product.html", context)
