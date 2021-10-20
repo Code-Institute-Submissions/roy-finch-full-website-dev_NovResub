c6fbda1158e4b65b2bfa20817cf212f07b9b3745
roy-finch <royfinch99@hotmail.com>
2021-10-20 19:42:32 +0000

    added a basic checkout render function in the views file, also added the models classes and the urls to get to the checkout

---
+++ checkout/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def checkout(request):
+    """
+    This sets up the form for checking out
+    and also sets up the stripe payment and view
+    """
+    order_form = OrderForm()
+
+    context = {
+        "order_form": order_form,
+    }
+
+    return render(request, "checkout/checkout.html", context)
