c39984defc3d021f52d367e69e6f9ea7ee9c7f86
roy-finch <royfinch99@hotmail.com>
2021-10-22 16:29:22 +0000

    added the profiles view and the html

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile
+from .forms import ProfileForm
 
-# Create your views here.
+
+@login_required
+def profile(request):
+    """
+    This is to render the profile
+    """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == "POST":
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile update successfully')
+        else:
+            messages.error(request, "Unable to update your profile")
+    else:
+        form = ProfileForm(instance=profile)
+    orders = profile.orders.all()
+
+    template = 'profiles/profiles.html'
+    context = {
+        "profile": profile,
+        "form": form,
+        "orders": orders,
+        "check_display": False
+    }
+
+    return render(request, template, context)
