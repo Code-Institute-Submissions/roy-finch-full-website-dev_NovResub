50f31bc165f48585130ee0b62ad23a17db7c96b5
roy-finch <royfinch99@hotmail.com>
2021-10-24 19:54:44 +0000

    altering settings, adding procfile, adding DISABLE_STATIC and adding allowed hosts.

--- retro_exchange/settings.py
+++ retro_exchange/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-ppj!8w@9*s8*8*57j79)9gnv3+pq8x24-xf1_pyyhcyr=4c2r*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['retro-exchange-warr', 'localhost']
 
 
 # Application definition
@@ -117,12 +117,17 @@ WSGI_APPLICATION = 'retro_exchange.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
