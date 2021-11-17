810a8a33eedd2e81b25e588b86a3aa4a92155d20
roy-finch <royfinch99@hotmail.com>
2021-11-17 00:29:37 +0000

    testing the email on signup and also changing style of the add products page.

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .widget import CustomFileInput
-from .models import Product, Category
+from .models import Product, Category, Console
 
 
 class ProductForm(forms.ModelForm):
@@ -19,6 +19,22 @@ class ProductForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        for field in self.fields:
+            self.fields[field].widget.attrs["class"] = "w-100 mb-3 p-2"
+
+
+class ConsoleForm(forms.ModelForm):
+    """
+    Form for the products when adding and removing
+    products from the website
+    """
+    class Meta:
+        model = Console
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         for field in self.fields:
             self.fields[field].widget.attrs["class"] = "w-100 mb-3 p-2"
